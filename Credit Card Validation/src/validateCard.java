aee322b4e70d945c2c7b13d03f3e17bde30bb45e
thomas.bradac <thomas.bradac@MU-C1-10.mullen.pvt.k12.co.us>
2026-03-05 12:15:05 -0700

    user input card check works. need to loop through arraylist of card numbers now and check those. 3/5/26

--- Credit Card Validation/src/validateCard.java
+++ Credit Card Validation/src/validateCard.java
@@ -17,12 +17,12 @@ public class validateCard
 				if (choice == 1)
 					{
 						userCheck();
-						cardTest();
+						System.out.println(cardTest(cardNumber));
 					}
 				else if (choice == 2)
 					{
 						readTextFile();
-						cardTest();
+						cardTest(cardNumber);
 					}
 				else
 					{
@@ -34,7 +34,7 @@ public class validateCard
 		{
 			Scanner userLongInput = new Scanner(System.in);
 			System.out.println("Please enter the credit card number to see if it's valid.");
-			Long card = userLongInput.nextLong();
+			cardNumber = userLongInput.nextLong();
 		}
 		
 		public static void readTextFile() throws IOException
@@ -52,21 +52,45 @@ public class validateCard
 		
 		public static boolean cardTest(long cardNumber)
 		{
+			//fill array with CC#
+			String cardString = String.valueOf(cardNumber);
 			int[] digits = new int[16];
 			for (int i = 0; i < 16; i++)
 				{
-					
+					digits[i] = Character.getNumericValue(cardString.charAt(i));
 				}
 			
+			//Step 1. Double alternating digits starting with the first digit in the sequence.
+			for (int i = 0; i < 16; i+=2)
+				{
+					digits[i] = digits[i] * 2;
+			//Step 2. If the doubling resulted in a number with two digits, add them together to get a single digit number
+					if (digits[i] >= 10)
+						{
+							String digitString = String.valueOf(digits[i]);
+							int firstDigit = digitString.charAt(0);
+							int secondDigit = digitString.charAt(1);
+							digits[i] = firstDigit + secondDigit;		
+						}
+				}
 			
+			//Step 4. Check to see if the sum is evenly divisible by 10 (you can simply look to see whether or not it ends with a zero).
+			long finalNumber = 0;
+			for (int i = 0; i < 16; i+=2)
+				{
+					finalNumber = (finalNumber * 10) + digits[i]; 
 			
-			
+				}
+			if (finalNumber % 10 == 0)
+				{
+					return true;
+				}
 			
 			return false;
 		}
-		{
+		
 			
-		}
+		
 		
 		//Step 1. Double alternating digits starting with the first digit in the sequence.
 		//Step 2. If the doubling resulted in a number with two digits, add them together to get a single digit number
